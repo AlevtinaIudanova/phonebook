f8e6a818c3abec6356677cc32c3e75444bbdd2bb
AlevtinaIudanova <iudanova87@bk.ru>
2024-02-06 10:26:12 +0100

    data_create

---
+++ data_create.py
@@ -0,0 +1,37 @@
+def name_data():
+    name = input('Введите ваше имя: ')
+    print('Очень красивое имя! ')
+    return name
+
+def new_name_data():
+    new_name = input('Введите новое имя: ')
+    print('Очень красивое имя! ')
+    return new_name
+
+def surname_data():
+    surname = input('Введите вашу фамилию: ')
+    return surname
+
+def new_surname_data():
+    new_surname = input('Введите новую фамилию: ')
+    return new_surname
+
+def phone_data():
+    phone = int(input('Введите ваш телефон: '))
+    return phone
+
+def new_phone_data():
+    new_phone = int(input('Введите новый телефон: '))
+    return new_phone
+
+def address_data():
+    address = input('Введите ваш адрес: ')
+    return address
+
+def new_address_data():
+    new_address = input('Введите новый адрес: ')
+    return new_address
+
+
+
+
